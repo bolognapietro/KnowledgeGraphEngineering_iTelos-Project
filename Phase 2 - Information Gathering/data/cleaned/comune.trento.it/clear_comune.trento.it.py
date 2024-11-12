eff15b666f8ca26feeb6b785e7760b9e22c0c1a6
bolognapietro <pietro.bologna@studenti.unitn.it>
2024-11-12 16:46:00 +0100

    clean comune.trento.it

--- Phase 2 - Information Gathering/data/cleaned/comune.trento.it/clear_comune.trento.it.py
+++ Phase 2 - Information Gathering/data/cleaned/comune.trento.it/clear_comune.trento.it.py
@@ -23,15 +23,41 @@ def copy_files(src_dir, dest_dir):
             with open(file_path, mode='w', encoding='utf-8') as file:
                 file.write(content)
 
-def get_headers_from_files(directory):
+def rename_headers(directory):
     headers = {}
+    headers_dict = {
+        "name": "legalName",
+        "website": "url",
+        "phone": "telephone",
+    }
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         if os.path.isfile(file_path) and filename.endswith('.csv'):
             with open(file_path, mode='r', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 headers[filename] = next(reader)
-    return headers
+                new_headers = [headers_dict.get(header, header) for header in headers[filename]]
+                rows = list(reader)
+                with open(file_path, mode='w', encoding='utf-8', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(new_headers)
+                    writer.writerows(rows)
+
+def remove_cols(directory, col_name):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path) and filename.endswith('.csv'):
+            with open(file_path, mode='r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                headers = next(reader)
+                if col_name in headers:
+                    col_index = headers.index(col_name)
+                    rows = [row[:col_index] + row[col_index+1:] for row in reader]
+                    headers.pop(col_index)
+                    with open(file_path, mode='w', encoding='utf-8', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(headers)
+                        writer.writerows(rows)
 
 # Copy files from raw to cleaned
 input_path = "Phase 2 - Information Gathering/data/raw/comune.trento.it/csv"
@@ -39,6 +65,7 @@ output_path = "Phase 2 - Information Gathering/data/cleaned/comune.trento.it/csv
 copy_files(input_path, output_path)
 
 # Get headers from files
-headers = get_headers_from_files(output_path)
-for file, header in headers.items():
-    print(f"Header for {file}: {header}")
+rename_headers(output_path)
+
+# Remove 'descriptions' column from files
+remove_cols(output_path, 'description')
