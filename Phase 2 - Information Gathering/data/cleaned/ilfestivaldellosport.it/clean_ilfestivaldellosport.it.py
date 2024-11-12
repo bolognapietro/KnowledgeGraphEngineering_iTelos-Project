6439370a442bed678b66c29b0994ac39922b0dbb
bolognapietro <pietro.bologna@studenti.unitn.it>
2024-11-12 17:17:27 +0100

    clean paginegialle.it & ilfestivaldellosport.it

--- Phase 2 - Information Gathering/data/cleaned/ilfestivaldellosport.it/clean_ilfestivaldellosport.it.py
+++ Phase 2 - Information Gathering/data/cleaned/ilfestivaldellosport.it/clean_ilfestivaldellosport.it.py
@@ -23,11 +23,7 @@ def copy_files(src_dir, dest_dir):
 def rename_headers(directory):
     headers = {}
     headers_dict = {
-        "name": "legalName",
-        "website": "url",
-        "comune": "municipaliy",
-        "open_hours": "openingHours",
-        "phone_number": "telephone",
+        "date": "startDate",
     }
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
@@ -42,10 +38,33 @@ def rename_headers(directory):
                     writer.writerow(new_headers)
                     writer.writerows(rows)
 
+def edit_guest(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path) and filename.endswith('.csv'):
+            with open(file_path, mode='r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                headers = next(reader)
+                rows = list(reader)
+                
+            guest_index = headers.index('guests') if 'guests' in headers else None
+            if guest_index is not None:
+                for row in rows:
+                    row[guest_index] = row[guest_index].strip("[]").replace("'", "")
+                    
+            with open(file_path, mode='w', encoding='utf-8', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(headers)
+                writer.writerows(rows)
+
+
 # Copy files from raw to cleaned
-input_path = "Phase 2 - Information Gathering/data/raw/paginegialle.it/csv"
-output_path = "Phase 2 - Information Gathering/data/cleaned/paginegialle.it/csv"
+input_path = "Phase 2 - Information Gathering/data/raw/ilfestivaldellosport.it/csv"
+output_path = "Phase 2 - Information Gathering/data/cleaned/ilfestivaldellosport.it/csv"
 copy_files(input_path, output_path)
 
 # Get headers from files
 rename_headers(output_path)
+
+# Edit guest column
+edit_guest(output_path)
